f3ce3910eb4cc3a3ca1a6bdb27478ecb55b176ef
Marc Gueury <marc.gueury@oracle.com>
2026-05-22 17:18:57 +0200

    v3.0

--- src/terraform/openid.tf
+++ src/terraform/openid.tf
@@ -1,58 +1,64 @@
+variable openid {
+    description="enable OpenID (SSO) if true"
+    default=null
+}
+
 resource "oci_identity_domains_app" "starter_confidential_app" {
-  active                  = "true"
-  all_url_schemes_allowed = "false"
-  allow_access_control    = "false"
-  allowed_grants = [
-    "client_credentials",
-    "authorization_code",
-  ]
-  allowed_operations = [
-    "introspect",
-  ]
-  attr_rendering_metadata {
-    name = "aliasApps"
-    section = ""
-    visible = "false"
-    widget  = ""
-  }
-  based_on_template {
-    value         = "CustomWebAppTemplateId"
-    well_known_id = "CustomWebAppTemplateId"
-  }
-  client_ip_checking = "anywhere"
-  client_type        = "confidential"
-  delegated_service_names = [
-  ]
-  display_name = "${var.prefix}-confidential-app"
-  idcs_endpoint = "${local.local_idcs_url}"
-  is_alias_app      = "false"
-  is_enterprise_app = "false"
-  is_kerberos_realm = "false"
-  is_login_target   = "true"
-  is_mobile_target  = "false"
-  is_oauth_client   = "true"
-  is_oauth_resource = "false"
-  is_saml_service_provider = "false"
-  is_unmanaged_app         = "false"
-  is_web_tier_policy       = "false"
-  login_mechanism = "OIDC"
-  # logout_uri = "https://${local.local_apigw_hostname}/logout"
-  post_logout_redirect_uris = [
-    "https://www.oracle.com"
-#    "https://${local.local_apigw_hostname}/"
-  ]
-  redirect_uris = [
-      "https://localhost/api/auth/callback/oci"
-#     "https://${local.local_apigw_hostname}/api/auth/callback/oci"
-  ]
-  schemas = [
-    "urn:ietf:params:scim:schemas:oracle:idcs:App"
-  ]
-  show_in_my_apps = "false"
-  trust_scope     = "Account"
+    count                   = var.openid=="true" ? 0 : 1      0
+    active                  = "true"
+    all_url_schemes_allowed = "false"
+    allow_access_control    = "false"
+    allowed_grants = [
+        "client_credentials",
+        "authorization_code",
+    ]
+    allowed_operations = [
+        "introspect",
+    ]
+    attr_rendering_metadata {
+        name = "aliasApps"
+        section = ""
+        visible = "false"
+        widget  = ""
+    }
+    based_on_template {
+        value         = "CustomWebAppTemplateId"
+        well_known_id = "CustomWebAppTemplateId"
+    }
+    client_ip_checking = "anywhere"
+    client_type        = "confidential"
+    delegated_service_names = [
+    ]
+    display_name = "${var.prefix}-confidential-app"
+    idcs_endpoint = "${local.local_idcs_url}"
+    is_alias_app      = "false"
+    is_enterprise_app = "false"
+    is_kerberos_realm = "false"
+    is_login_target   = "true"
+    is_mobile_target  = "false"
+    is_oauth_client   = "true"
+    is_oauth_resource = "false"
+    is_saml_service_provider = "false"
+    is_unmanaged_app         = "false"
+    is_web_tier_policy       = "false"
+    login_mechanism = "OIDC"
+    logout_uri = "https://${local.local_apigw_hostname}/logout"
+    post_logout_redirect_uris = [
+        "https://www.oracle.com",
+        "https://${local.local_apigw_hostname}/"
+    ]
+    redirect_uris = [
+        "https://localhost/api/auth/callback/oci",
+        "https://${local.local_apigw_hostname}/api/auth/callback/oci"
+    ]
+    schemas = [
+        "urn:ietf:params:scim:schemas:oracle:idcs:App"
+    ]
+    show_in_my_apps = "false"
+    trust_scope     = "Account"
 }
 
 locals {
-  openid_client_id = oci_identity_domains_app.starter_confidential_app.name
-  openid_client_secret = oci_identity_domains_app.starter_confidential_app.client_secret
+    openid_client_id = try(oci_identity_domains_app.starter_confidential_app.name, "")
+    openid_client_secret = try(oci_identity_domains_app.starter_confidential_app.client_secret, "")
 }
